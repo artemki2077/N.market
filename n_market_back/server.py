c61c8e14aeb76cf16d3e65c8c34c753b3e33bbcd
Artem Kiselev <86531080+artemki2077@users.noreply.github.com>
2022-11-30 07:48:26 +0000

    add on real it

--- n_market_back/server.py
+++ n_market_back/server.py
@@ -1,11 +1,13 @@
 from typing import Union
-
-
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
-
+@app.get("/")
+def read_root():
+    return 'lol'
+	
 @app.get("/update")
 def read_root():
     return {"Hello": "World"}
@@ -14,3 +16,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
